aa48a38a88b7c5dab5b7ae84007c12c7e0d9de62
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-04-20 17:57:22 -0500

    configured the database contection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //cambio en la entidad la sincriniza, no usar en produccion
+    }),
+    ProductModule,
+  ],
 })
 export class AppModule {}
